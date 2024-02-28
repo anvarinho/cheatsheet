e683dc81316e94057ac25f749ad93180a23376c8
anvarinho <anvarinho@gmail.com>
2024-02-28 22:40:46 +0600

    added bashscripts.md cheatsheet

---
+++ script.sh
@@ -0,0 +1,10 @@
+user=$(whoami)
+date=$(date)
+dir=$(pwd)
+name=$1
+comp=$2
+echo "Hello $user!"
+sleep 1
+echo "You are looking great $name, now is $date!!!!!"
+sleep 1
+echo "$1, you have a best $comp I've ever seen. You are in $dir directory"
